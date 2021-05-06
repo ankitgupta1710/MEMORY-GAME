7730b15abbde6f34591da7ee5643481c4a699351
ankitgupta1710 <ankit.gupta18@st.niituniversity.in>
2021-05-06 14:44:40 +0530

    Initial V 1.0

---
+++ src/app/service/serv.service.ts
@@ -0,0 +1,122 @@
+import { Injectable } from '@angular/core';
+// import { Audio1 } from '../Audio/1.wav';
+@Injectable({
+  providedIn: 'root'
+})
+export class ServService {
+  tiles: number = 1;
+  colorBlockAll: Array<Object> = [{ color: "white", animate: "large", audio: new Audio() }, { color: "violet", animate: "large", audio: new Audio() }, { color: "pink", animate: "large", audio: new Audio() }, { color: "red", animate: "large", audio: new Audio() }, { color: "orange", animate: "large", audio: new Audio() }, { color: "green", animate: "large", audio: new Audio() }, { color: "yellow", animate: "large", audio: new Audio() }, { color: "blue", animate: "large", audio: new Audio() }]
+  colorBlock: Array<Object> = []
+  colorSeries: Array<number> = [];
+  level: number = 1;
+  index: number = 0;
+  lastClicked: number;
+  playerChoise: Array<number> = [];
+  addLevelFlag: boolean = true;
+  clickMulti: boolean = true;
+  hideStartButton: boolean = true;
+  playingAuth: boolean = false;
+  failSound: any = new Audio();
+  buttonClick: any = new Audio();
+  timeWait: number = 1500;
+  showLose: boolean = false;
+  color: Array<string> = ['Pink','Violet','White','RED', 'Orange', 'Green','Yellow','Blue'];
+  lastcolor: string;
+  uturn : boolean = false;
+
+  constructor() {
+    this.loadSounds();
+  }
+  loadSounds(): void {
+    this.tiles = +(window.prompt("Enter number of tiles:  "));
+    this.colorBlock = this.colorBlockAll.slice(1, this.tiles + 1);
+    for (const key in this.colorBlock) {
+      this.colorBlock[key]['audio'].src = `./../assets/Audio/${key}.wav`;
+      this.colorBlock[key]['audio'].load();
+    }
+    this.failSound.src = `./../assets/Audio/fail.mp3`;
+    this.failSound.load()
+    this.buttonClick.src = `./../assets/Audio/button.mp3`;
+    this.buttonClick.load();
+  }
+
+  runGame(): void {
+    
+    this.failSound.pause();
+    this.hideStartButton = false, this.playingAuth = false;
+    this.uturn = false;
+    this.getNewIndex();
+    setTimeout(() => {
+      if (this.index < this.colorSeries.length) {
+        this.animateMe(this.colorSeries[this.index]);
+        this.index++;
+        this.runGame();
+      }
+      else {
+        this.playingAuth = true;
+        this.uturn = true;
+        this.index = 0;
+        this.addLevelFlag = true;
+      }
+    }, this.timeWait)
+  }
+
+  animateMe(color: number): void {
+    this.lastClicked ? this.colorBlock[this.lastClicked]['audio'].pause() : undefined;
+    this.colorBlock[color]['audio'].play();
+    this.colorBlock[color]['animate'] = "small";
+    this.clickMulti = false;
+    setTimeout(() => {
+      this.lastClicked = color;
+      this.colorBlock[color]['animate'] = "large";
+      this.clickMulti = true;
+    }, 500);
+  }
+  getNewIndex(): void {
+    if (this.addLevelFlag) {
+      this.colorSeries.push(Math.floor((Math.random() * this.tiles) + 0));
+      this.addLevelFlag = false;
+      return
+    }
+  }
+
+  addPlayerChoise(Choise: number): any {
+    this.animateMe(Choise);
+    this.playerChoise.push(Choise);
+    this.lastcolor= this.color[Choise]
+    this.checkChoisePath(Choise) ? (this.playerChoise.length == this.colorSeries.length) ? this.levelUp() : undefined : this.restLevel();
+  }
+
+  checkChoisePath(Choise: number): boolean {
+    return (this.colorSeries[this.playerChoise.length - 1] == Choise);
+  }
+
+  restLevel() {
+    this.colorSeries = [];
+    this.playerChoise = [];
+    this.hideStartButton = true;
+    this.playingAuth = false;
+    this.uturn = false;
+    this.failSound.play();
+    this.showLose = true;
+    setTimeout(() => {
+      this.showLose = false;
+    }, 2500);
+  }
+
+  reset() {
+    this.colorSeries = [];
+    this.playerChoise = [];
+    this.hideStartButton = true;
+    this.playingAuth = false;
+    this.uturn = false;
+    
+  }
+
+  levelUp() {
+    this.playerChoise = [];
+    this.timeWait -= 10;
+    this.runGame();
+  }
+
+}
